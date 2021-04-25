61a57a389a00ea6c9b6e6f42593b0725254b09f8
Felix.MULLER <felix.muller@ens.uvsq.fr>
2021-04-25 17:43:02 +0200

    Partie de Felix.

--- avion_groupe_4.py
+++ avion_groupe_4.py
@@ -20,32 +20,56 @@ HAUTEUR = 280
 LARGEUR = 1200
 COTE = 40 # Taille du côté, doit être un diviseur commun de la largeur et de la hauteur
 COULEURS = ["white","grey"]
+Places = []
+Couloir = []
 
-""" Créer liste de listes où nous avons pour chaque passager
+""" Crée liste de listes où nous avons pour chaque passager
 des coordonnées (endroit où il est) et une destination (endroit où il va)
-ainsi qu'un nombre de bagage tiré aléatoirement entre 1 et 2"""
-liste = [ ]
+ainsi qu'un nombre de bagage tiré aléatoirement entre 1 et 2 de type 
+Passagers = [[coordonée_x, coordonnée_y, destination_x, destination_y, nb_baggage], ...]"""
+Passagers = [ ]
 
 def gen_terrain():
-    """Fonction permettant de générer le terrain. La liste Cells contient les coordonnées de chaques cellules ainsi que leurs couleurs respective, leur DUREE_FEU et DUREE_CENDRE"""
+    """Fonction permettant de générer le terrain. La liste Couloir
+    contient les coordonnées de chaques cellules du couloir et ses couleurs et la liste Places 
+    contient les coordonnées des places disponibles"""
 
-    global Cells
-    Cells = []
+    global Couloir
+    global Places
     terrain.delete("all")
 
     for i in range(1, LARGEUR, COTE):
         for j in range(1, HAUTEUR, COTE):
-            Cells.append([i, j, 0])
-    for n in range(len(Cells)):
-          if Cells[n][1] == HAUTEUR/2 - COTE/2 + 1:
-               terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+COTE, Cells[n][1]+COTE, fill="white")
-          else:
-               terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+COTE, Cells[n][1]+COTE, fill="grey")
-
+            if j == HAUTEUR/2 - COTE/2 + 1:
+                Couloir.append([i, j, "white"])
+                terrain.create_rectangle(i, j, i+COTE, j+COTE, fill="white")
+            else:
+                terrain.create_rectangle(i, j, i+COTE, j+COTE, fill="grey")
+                Places.append([i, j, "grey"])
+                
+                
+#Cette fonction serve a passer a une nouvelle etape. Elle cree un nouveau passager si la parcelle d'entree est 
+#disponible et il reste des places libres. Elle sert aussi a deplacer les passagers dans l'avion.
+def nouvelle_etape():
+    global Passagers
+    #boucle qui deplace chaque passagers vers sa place.
+    if len(Places) > 0 and Couloir[0][2] == "white":
+        destination = rd.choice(Places)
+        Places.remove(destination)
+        Passagers.append([Couloir[0][0], Couloir[0][1], destination[0], destination[1], rd.randint(0, 2)])
+        
+                
+                
+                
 racine = tk.Tk()
 racine.title("Simulation d'entrées de voyageurs dans un avion")
+
 terrain = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR, bg="black")
 Bouton_terrain = tk.Button(racine, text="Generate Terrain", command=gen_terrain)
-terrain.grid(row=1, column=0)
-gen_terrain()
+Bouton_nouvelle_etape = tk.Button(racine, text = "Prochaine étape", command = nouvelle_etape)
+
+terrain.grid(row = 2, columnspan = 2)
+Bouton_terrain.grid(row = 0, column = 0)
+Bouton_nouvelle_etape.grid(row = 0, column = 1)
+
 racine.mainloop()

b9f52ff809e81b88bc9de2ac8c453ec24592691f
Lucas FEDERSPIEL <lucas.federspiel@ens.uvsq.fr>
2021-04-28 11:30:39 +0200

    ajout du mouvement des passagers (pas terminé à 100%)

--- avion_groupe_4.py
+++ avion_groupe_4.py
@@ -26,6 +26,7 @@ Passagers = []
 Places_dispo = []
 
 
+
 """ Crée liste de listes où nous avons pour chaque passager
 des coordonnées (endroit où il est) et une destination (endroit où il va)
 ainsi qu'un nombre de bagage tiré aléatoirement entre 1 et 2 de type 
@@ -56,16 +57,114 @@ def gen_terrain():
 #Cette fonction serve a passer a une nouvelle etape. Elle cree un nouveau passager si la parcelle d'entree est 
 #disponible et il reste des places libres. Elle sert aussi a deplacer les passagers dans l'avion.
 def nouvelle_etape():
-    global Passagers
+    global Passagers, Couloir
     global Places_dispo
+
+    move()
     #boucle qui deplace chaque passagers vers sa place.
     if len(Places_dispo) > 0 and Couloir[0][2] == "white":
         destination = rd.choice(Places_dispo)
         Places_dispo.remove(destination)
         Passagers.append([Couloir[0][0], Couloir[0][1], destination[0], destination[1], rd.randint(0, 2)])
-        print(destination)
+        print(Passagers)
+    
+    nb_passager = 0
 
-                
+    for i in range(len(Passagers)):
+        if Passagers[i][0] == Couloir[0][0] and Passagers[i][1] == Couloir[0][1]:
+            Couloir[0][2] = "blue"
+        else:
+            nb_passager += 1
+    
+    if nb_passager == len(Passagers):
+        Couloir[0][2] = "white"
+
+    print(Couloir[0])
+
+    terrain.after(40, nouvelle_etape)
+
+
+### Sort de la liste Passagers (plus grand x au début) -> mouvement de gauche a droite pour eviter les blocages
+
+def move():
+
+    global Passagers
+
+    terrain.delete("all")
+
+    for i in range(1, LARGEUR, COTE):
+        for j in range(1, HAUTEUR, COTE):
+            if j == HAUTEUR/2 - COTE/2 + 1:
+                terrain.create_rectangle(i, j, i+COTE, j+COTE, fill="white")
+            else:
+                terrain.create_rectangle(i, j, i+COTE, j+COTE, fill="grey")
+
+    sort_list(Passagers)
+    for i in range(len(Passagers)):
+        n = 0
+    
+        if Passagers[i][0] < Passagers[i][2]:
+
+            for j in range(len(Passagers)):
+                if Passagers[i][1] == Couloir[0][1] and Passagers[i][0] + COTE == Passagers[j][0] and Passagers[j][1] == Couloir[0][1]:
+                    n += 1
+
+
+                elif Passagers[i][1] == Couloir[0][1] and Passagers[i][0] + COTE != Passagers[j][0]:
+                    pass
+            
+            if n >= 1:
+                pass
+
+            elif n == 0 and Passagers[i][0] <= Passagers[i][2]:
+                Passagers[i][0] += COTE
+
+        elif Passagers[i][0] == Passagers[i][2] and Passagers[i][4] > 0:
+
+            Passagers[i][4] -= 1
+
+        elif Passagers[i][0] == Passagers[i][2] and Passagers[i][4] == 0 and Passagers[i][1] != Passagers[i][3]:
+
+            if Passagers[i][1] < Passagers[i][3]:
+                bloque = False
+                for k in range(len(Passagers)):
+
+                    if Passagers[i][1] + COTE == Passagers[k][1] and Passagers[i][0] == Passagers[k][0]:
+                        Passagers[i][1] += COTE
+                        Passagers[k][1] -= COTE
+                        bloque = True
+
+                if bloque == False:
+                    Passagers[i][1] += COTE
+                    
+
+            elif Passagers[i][1] > Passagers[i][3]:
+                bloque = False
+                for k in range(len(Passagers)):
+
+                    if Passagers[i][1] - COTE == Passagers[k][1] and Passagers[i][0] == Passagers[k][0]:
+                        Passagers[i][1] -= COTE
+                        Passagers[k][1] += COTE
+                        bloque = True
+
+                if bloque == False:
+                    Passagers[i][1] -= COTE
+                    
+
+    
+
+    for a in range(len(Passagers)):
+        terrain.create_oval(Passagers[a][0], Passagers[a][1], Passagers[a][0] + COTE, Passagers[a][1] + COTE, fill="blue")
+
+
+
+def sort_list(liste):
+    """Permet de trier les éléments d'une liste selon les coordonées x de facons décroissante"""
+
+    liste.sort(key = lambda x : x[0])
+    liste.reverse()
+
+
                 
                 
 racine = tk.Tk()
